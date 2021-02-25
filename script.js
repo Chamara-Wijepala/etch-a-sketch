e9c89a62fb30d5fa4bd1950209dcb38b7939a8ea
Chamara-Wijepala <mr.worf777@gmail.com>
2021-02-25 18:33:16 +0530

    Add button that allows user to change grid size

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const GRID_SIZE = 16 * 16;
+let side = 16;
+const gridContainer = document.getElementById('grid');
 
 function createGrid() {
-
-    const gridContainer = document.getElementById("grid");
+    let gridSize = side * side;
     
-    for (let i = 0; i < GRID_SIZE; i++) {
-        const block = document.createElement("div");
-        block.className = "gridBlock";
-
+    for (let i = 0; i < gridSize; i++) {
+        const block = document.createElement('div');
+        block.className = 'gridBlock';
         gridContainer.appendChild(block);
     };
+
+    blockListeners();
 };
 
-createGrid();
+function removeGrid() {
+    const gridContainerArray = Array.from(gridContainer.childNodes);
+
+    gridContainerArray.forEach((child) => {
+        gridContainer.removeChild(child);
+    });
+
+    createGrid();
+};
 
 function generateRandomColor() {
     let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
@@ -23,16 +32,36 @@ function colorChange() {
     this.style.backgroundColor = generateRandomColor();
 };
 
-const gridBlock = document.querySelectorAll(".gridBlock");
-gridBlock.forEach(div => {
-    div.addEventListener('mouseover', colorChange);
-});
+function blockListeners() {
+    document.querySelectorAll('.gridBlock').forEach(div => {
+        div.addEventListener('mouseover', colorChange);
+    });
+};
 
 function clearGrid() {
-    gridBlock.forEach(div => {
+    document.querySelectorAll('.gridBlock').forEach(div => {
         div.style.backgroundColor = '#FFFFFF';
     });
 };
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", clearGrid);
+document.querySelector('#clear').addEventListener('click', clearGrid);
+
+function changeGridSize(blocksPerSide) {
+    //let blocksPerSide = parseInt(window.prompt('How many squares on each side?'));
+    side = blocksPerSide;
+    document.documentElement.style.setProperty('--grid-size', `repeat(${side}, 1fr)`);
+    removeGrid();
+};
+
+function getGridSize() {
+    let blocksPerSide = parseInt(window.prompt('How many squares on each side? Max 100'));
+    
+    if (blocksPerSide < 0 || blocksPerSide > 100) {
+        getGridSize();
+    }
+    changeGridSize(blocksPerSide);
+};
+
+document.getElementById('size').addEventListener('click', getGridSize);
+
+createGrid();
